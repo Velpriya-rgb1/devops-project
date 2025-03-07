ebd06d23b69da6c18f4eccce8fe564cafe1c47d4
Velpriya <velpriyav.ug.21.it@francisxavier.ac.in>
2025-03-07 08:23:41 +0530

    Create server.js
    "Added server.js"

---
+++ server.js
@@ -0,0 +1,44 @@
+// server.js
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("Hello, DevOps! 🚀"));
+app.listen(3000, () => console.log("Server running on port 3000"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
